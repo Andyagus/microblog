3db0a16d5014ba1f244fee84288a7cd8ffcbe532
Andy Agus <Redagus@gmail.com>
2015-07-02 13:45:23 -0400

    Added Heroku Hosting and sign-in page

--- app.rb
+++ app.rb
@@ -5,12 +5,25 @@ require 'rack-flash'
 require './models'
 
 set :database, "sqlite3:microblog.sqlite3"
+configure(:development) { set :database, "sqlite3:microblog.sqlite3"}
+
+enable :sessions
+
+use Rack::Flash, sweep: true
+
+
+## SIGN UP SECTION
 
 get '/sign-up' do
    erb :sign_up
 end
 
 post '/sign-up' do 
+
+   username = params[:username]
+   email = params[:email]
+   birthday = params[:birthday]
+   password = params[:password]
    confirmation = params[:confirm_password]
 
    if confirmation == params[:user][:password]
@@ -19,5 +32,54 @@ post '/sign-up' do
    else
       "Your passwords do not match, please try again"
    end
+end
+
+# SIGN IN SECTION
+
+get "/" do
+   if current_user
+      @username = params[:username]
+      erb :index
+   else
+      redirect '/sign-up'
+   end
+end
+
+
+get '/sign-in' do
+   erb :sign_in
+end   
+
+post '/sign-in' do
+   username = params[:username]
+   password = params[:password]
+
+   @user = User.where(username: username).first
+
+   if @user.password == password
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome #{@user.username}!"
+      redirect '/'
+   else
+      flash[:notice] = "Wrong login info, please try again"
+      redirect '/sign-up'
+   end
+end
+
+def current_user
+   if session[:user_id]
+      User.find session[:user_id]
+   end
+end
+
+get '/profile-form' do
+   
+end
+
+post '/profile-form' do
+end
+
+get '/profile' do
+   #{@user.username}
 end
 
